5079f1cbed22a23a640806e35d5d51fdbba835d5
holy <holy>
2017-07-28 16:59:34 +0800

    377

---
+++ 377.h
@@ -0,0 +1,14 @@
+377. Combination Sum IV
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto num:nums){
+                if(num<=i) dp[i]+=dp[i-num];
+            }
+        }
+        return dp.back();
+    }
+};
